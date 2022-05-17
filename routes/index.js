c44e8c476735ee63d80f6a3db227000c1dc3d5ea
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-05-17 19:10:14 -0300

    req 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs').promises;
 const express = require('express');
 
-const talkerJson = 'talker.json';
 const routes = express.Router();
+const talkerJson = 'talker.json';
 
 const HTTP_OK_STATUS = 200;
 
@@ -11,10 +11,10 @@ routes.get('/talk', async (request, response) => {
     const talker = JSON.parse(rotaDados);
 
     if (!talker) {
-        response.status(HTTP_OK_STATUS).json(talker);
+        return response.status(HTTP_OK_STATUS).json(talker);
     }
 
-    response.status(HTTP_OK_STATUS).json(talker);
+    return response.status(HTTP_OK_STATUS).json(talker);
   });
 
   module.exports = routes;
