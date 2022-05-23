70c400f8ba9af8da725ace0d0bbf0f487adbdfc4
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-05-23 12:13:24 -0300

    requisito 6

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const agePost = require('./agePost');
 const talkPost = require('./talkPost');
 const talkWatchedAt = require('./talkWatchedAt');
 const talkRate = require('./talkRate');
+const { write } = require('fs');
 
 const talkerJson = 'talker.json';
 
@@ -69,24 +70,27 @@ async (req, res) => {
     }
 });
 
-// routes.put('/talker/:id', 
-// authMiddleware, namePost, agePost, talkWatchedAt, talkRate, async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         const { name, age, talk } = req.body;
-//         const rotaDados = await fs.readFile('talker.json');
-//         const talker = JSON.parse(rotaDados);
-        
-//         const talkerUser = talker.find((t) => t.id === parseInt(id, 10));
-//         const result = talkerUser.update(name, age, id, talk);
-//         await fs.update('talker.json', result);
-
-//         return res.status(201).json(result); 
-
-//     } catch (error) {
-
-//     }
-// });
+routes.put('/talker/:id', 
+authMiddleware, namePost, agePost, talkPost, talkWatchedAt, talkRate, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, age, talk } = req.body;
+        const rotaDados = await fs.readFile('talker.json');
+        const talker = JSON.parse(rotaDados); 
+        const talkerUserIndex = talker.findIndex((t) => t.id === parseInt(id, 10));
+        // talker[talkerUserIndex].id = talkerUserIndex;
+        talker[talkerUserIndex].name = name;
+        talker[talkerUserIndex].age = age;
+        talker[talkerUserIndex].talk.watchedAt = talk.watchedAt;
+        talker[talkerUserIndex].talk.rate = talk.rate;
+        // talker.splice(talkerUserIndex, 1);
+        const newTalker = [...talker, talker[talkerUserIndex]];
+        await fs.writeFile(talkerJson, JSON.stringify(newTalker));
+        return res.status(200).json(newTalker[talkerUserIndex]); 
+    } catch (error) {
+        return res.status(500).end();
+    }
+});
 
 routes.delete('/talker/:id', authMiddleware, async (req, res) => {
     const { id } = req.params;
--- routes/talkRate.js
+++ routes/talkRate.js
@@ -1,6 +1,6 @@
 const talkRate = (req, res, next) => {
         const { talk: { rate } } = req.body;
-        if (!rate || rate === undefined) {
+        if (rate === undefined) {
             return res.status(400).json({ message: 
                 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
         }
